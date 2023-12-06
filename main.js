eeca109ace29cdbf4d29de3ac030aee472df1943
raissa rodrigues <raissa.rodrigues@estufdante.ufjf.br>
2023-12-06 19:41:01 -0300

    adiciona novas funções ao main

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { getLista, limpaLista } from "./lista";
+import { adicionaNaLista, getLista, limpaLista, removeDaLista } from "./lista.js";
 
 const olItens = document.querySelector ("#itens");
 const pEntrada = document.querySelector ("#entrada");
@@ -6,20 +6,24 @@ const btnAdicionar = document.querySelector ("#adicionar");
 const btnLimpar = document.querySelector ("#limpar");
 
 btnLimpar.addEventListener ('click', limparItensDeLista);
+btnAdicionar.addEventListener ('click', criaElementoNaLista);
 
-function limparItensDeLista() {
+atualizarItensDeLista();
+
+function criaElementoNaLista() {
     limpaLista();
-    atualizarLista();
+    atualizarItensDeLista();
 }
 
-atualizarLista();
+function atualizarItensDeLista() {
+    olSaida.innerHTML = "";
+    const lista = getLista ();
 
-function atualizarLista() {
-    olItens.innerHTML = "";
-    let lista = getLista ();
     for (let i = 0; i < lista.length; i++){
+    const item = lista[i];
     const li = document.createElement('li');
-    li.textContent = lista[i];
-    olItens.appendChild(li);
+    li.textContent = item;
+
+    olSaida.appendChild(li);
  }
 }
