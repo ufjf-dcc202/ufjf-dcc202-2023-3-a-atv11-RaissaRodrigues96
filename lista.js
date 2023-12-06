23526b4f3c958b861bb46edf8e83ac86cb533059
raissa rodrigues <raissa.rodrigues@estufdante.ufjf.br>
2023-12-06 19:30:48 -0300

    add novas fuçoẽs a lista

--- lista.js
+++ lista.js
@@ -1,9 +1,19 @@
 const lista = ["Um", "Dois", "Três"];
 
 export function getLista() {
-    return structuredClone(lista);
+    return lista;
 }
+
+export {getLista};
 
 export function limpaLista() {
     lista.splice(0);
+}
+
+export function adicionaNaLista(novoItem){
+    lista.push(novoItem);
+}
+
+export function removeDaLista(posicao) {
+    lista.splice(posicao, 1);
 }
