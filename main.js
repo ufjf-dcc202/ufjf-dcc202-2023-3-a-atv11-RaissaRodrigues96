62aede9565814c6441de5d26712656ff20b23782
raissa rodrigues <raissa.rodrigues@estufdante.ufjf.br>
2023-12-06 18:59:33 -0300

    criando a função para atualizar a lista

--- main.js
+++ main.js
@@ -2,3 +2,7 @@ const olItens = document.querySelector ("#itens");
 const pEntrada = document.querySelector ("#entrada");
 const btnAdicionar = document.querySelector ("#adicionar");
 const btnLimpar = document.querySelector ("#limpar");
+
+function atualizarLista() {
+    
+}
