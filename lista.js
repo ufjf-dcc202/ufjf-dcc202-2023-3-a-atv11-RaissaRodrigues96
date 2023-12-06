1ab6f3982597c8cf0ba4dd19dee704f916f415ee
raissa rodrigues <raissa.rodrigues@estufdante.ufjf.br>
2023-12-06 20:22:27 -0300

    atualizações

--- lista.js
+++ lista.js
@@ -4,16 +4,16 @@ export function getLista() {
     return lista;
 }
 
-export {getLista};
-
 export function limpaLista() {
     lista.splice(0);
+}
+
+export function removeDaLista(posicao) {
+    lista.splice(posicao, 1);
 }
 
 export function adicionaNaLista(novoItem){
     lista.push(novoItem);
 }
 
-export function removeDaLista(posicao) {
-    lista.splice(posicao, 1);
-}
+export {getLista, removeDaLista, limpaLista, adicionaNaLista};
--- main.js
+++ main.js
@@ -1,29 +1,38 @@
-import { adicionaNaLista, getLista, limpaLista, removeDaLista } from "./lista.js";
+import { adicionaNaLista, getLista, limpaLista } from "./lista.js";
 
 const olItens = document.querySelector ("#itens");
 const pEntrada = document.querySelector ("#entrada");
 const btnAdicionar = document.querySelector ("#adicionar");
 const btnLimpar = document.querySelector ("#limpar");
 
-btnLimpar.addEventListener ('click', limparItensDeLista);
-btnAdicionar.addEventListener ('click', criaElementoNaLista);
+atualizarListaOrdenada();
 
-atualizarItensDeLista();
+btnLimpar.addEventListener ('click', limparListaOrdenada);
+btnAdicionar.addEventListener ('click', adicionaItemDeEntrada);
 
-function criaElementoNaLista() {
+function limparListaOrdenada() {
     limpaLista();
-    atualizarItensDeLista();
+    atualizarListaOrdenada();
 }
 
-function atualizarItensDeLista() {
-    olSaida.innerHTML = "";
-    const lista = getLista ();
+function adicionaItemDeEntrada () {
+    const valor = pEntrada.textContent
+    adicionaNaLista(valor);
+    atualizarItensDeLista();
+    pEntrada.textContent = ""
+    atualizarListaOrdenada();
+}
+
+function atualizarListaOrdenada() {
+    const lista=getLista();
+    olItens.innerHTML = "";
 
     for (let i = 0; i < lista.length; i++){
-    const item = lista[i];
-    const li = document.createElement('li');
-    li.textContent = item;
-
-    olSaida.appendChild(li);
- }
+        adicionaElementoNaListaOrdenada(lista[i]);
+    }
+}
+ 
+function adicionaElementoNaListaOrdenada(text){
+    limpaLista.textContent=texto;
+    olItens.appendChild(li);
 }
